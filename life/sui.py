ba984c24bf34ebe739d81b23b854b87fd515fadc
Paul Iutzi <pji@mac.com>
2020-02-08 18:42:49 -0600

    Added sui.Start.update_ui().

--- life/sui.py
+++ life/sui.py
@@ -27,7 +27,62 @@ class State(ABC):
         if not data:
             data = Grid(term.width, (term.height - 3) * 2)
         self.data = data
+    
+    def _char_for_state(self, top, bottom):
+        """Return the character to draw based on the state of the cell."""
+        if top and bottom:
+            return '\u2588'
+        if top and not bottom:
+            return '\u2580'
+        if not top and bottom:
+            return '\u2584'
+        if not top and not bottom:
+            return ' '
+    
+    def _draw_commands(self, cmds: str = None):
+        """Draw the available commands."""
+        if not cmds:
+            cmds = _Command.get_commands()
+        y = -(self.data.height // -2) + 1
+        print(self.term.move(y, 0) + ' ' * self.term.width)
+        print(self.term.move(y, 0) + cmds)
+    
+    def _draw_state(self):
+        """Draw the grid to the terminal."""
+        data = self.data[:]
+        if len(data) % 2:
+            data.append([False for _ in range(len(data[0]))])
+        for i in range(0, len(data), 2):
+            cells = []
+            for j in range(0, len(data[i])):
+                char = self._char_for_state(data[i][j], data[i+1][j])
+                cells.append(char)
+            print(self.term.move(i // 2, 0) + ''.join(cells))
+    
+    def _draw_prompt(self, msg: str = '> '):
+        """Draw the command prompt."""
+        y = -(self.data.height // -2) + 2
+        print(self.term.move(y, 0) + msg, end='')
+    
+    def _draw_rule(self):
+        """Draw the a horizontal rule."""
+        width = self.data.width
+        y = -(self.data.height // -2)
+        print(self.term.move(y, 0) + '\u2500' * width)
+    
+    @abstractmethod
+    def update_ui(self):
+        """Update the terminal display."""
 
 
 class Start(State):
     """The starting state for the UI."""
+    menu = 'Copyright © 2020 Paul J. Iutzi'
+    prompt = 'Press any key to continue.'
+    
+    def update_ui(self):
+        """Draw the initial display state."""
+        self._draw_state()
+        self._draw_rule()
+        self._draw_commands(self.menu)
+        self._draw_prompt(self.prompt)
--- tests/test_sui.py
+++ tests/test_sui.py
@@ -12,7 +12,18 @@ import blessed
 from life import grid, sui
 
 
+# Terminal control sequence templates.
+loc = '\x1b[{};{}H'
+
+
+# Test cases.
 class StartTestCase(ut.TestCase):
+    def _dicteq(self, expected, actual):
+        self.assertDictEqual(expected, actual)
+    
+    def _listeq(self, expected, actual):
+        self.assertListEqual(expected, actual)
+    
     def test_init_without_parameters(self):
         """Start.__init__() should create default attribute values 
         if no parameters are passed to it.
@@ -26,7 +37,7 @@ class StartTestCase(ut.TestCase):
             'data': type(state.data),
             'term': type(state.term),
         }
-        self.assertDictEqual(exp, act)
+        self._dicteq(exp, act)
     
     def test_init_with_parameters(self):
         """Start.__init__() should set the given parameters, as 
@@ -36,9 +47,29 @@ class StartTestCase(ut.TestCase):
             'data': grid.Grid(3, 3),
             'term': blessed.Terminal(),
         }
-        state = sui.State(**exp)
+        state = sui.Start(**exp)
         act = {
             'data': state.data,
             'term': state.term,
         }
-        self.assertDictEqual(exp, act)
+        self._dicteq(exp, act)
+    
+    @patch('life.sui.print')
+    def test_update_ui(self, mock_print):
+        """Start.update_ui() should draw the initial UI."""
+        data = grid.Grid(3, 3)
+        term = blessed.Terminal()
+        state = sui.Start(data, term)
+        exp = [
+            call(loc.format(1, 1) + '   '),
+            call(loc.format(2, 1) + '   '),
+            call(loc.format(3, 1) + '\u2500' * data.width),
+            call(loc.format(4, 1) + ' ' * term.width),
+            call(loc.format(4, 1) + state.menu),
+            call(loc.format(5, 1) + state.prompt, end=''),
+        ]
+        
+        state.update_ui()
+        act = mock_print.mock_calls
+        
+        self._listeq(exp, act)
