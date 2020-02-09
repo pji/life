3e97496e302e11423d004064b1da7059c720d951
Paul Iutzi <pji@mac.com>
2020-02-08 22:15:23 -0600

    Initial implementations of Core, End, and main() in sui.

--- life/sui.py
+++ life/sui.py
@@ -5,27 +5,29 @@ sui
 The user interface for Conway's Game of Life.
 """
 from abc import ABC, abstractmethod
+from typing import Any, Optional, Sequence, Tuple
 
 from blessed import Terminal
 
 from life.grid import Grid
 
 
+# Complex type aliases for type hints. See the following for more info:
+# https://docs.python.org/3/library/typing.html
+_ArgList = Sequence[Any]
+_Command = Tuple[str, Optional[_ArgList]]
+
+
+# Base class.
 class State(ABC):
     """An abstract base class for UI states."""
-    def __init__(self, data: Grid = None, term: Terminal = None):
+    def __init__(self, data:Grid, term:Terminal):
         """Initialize a State object.
         
-        :param data: (Optional.) The grid object for the current 
-            game of life.
-        :param term: (Optional.) The terminal the game of life is 
-            being run in.
+        :param data: The grid object for the current game of life.
+        :param term: The terminal the game of life is being run in.
         """
-        if not term:
-            term = Terminal()
         self.term = term
-        if not data:
-            data = Grid(term.width, (term.height - 3) * 2)
         self.data = data
     
     def _char_for_state(self, top, bottom):
@@ -71,8 +73,51 @@ class State(ABC):
         print(self.term.move(y, 0) + '\u2500' * width)
     
     @abstractmethod
+    def input(self) -> _Command:
+        """Get and handle input from the user."""
+    
+    @abstractmethod
     def update_ui(self):
         """Update the terminal display."""
+
+
+# State classes.
+class Core(State):
+    """The standard state of the UI. This is used to manually progress 
+    the grid and switch to other states.
+    """
+    menu = {
+        'q': 'quit',
+    }
+    
+    def input(self) -> _Command:
+        """Validate the user's command and return it."""
+        with self.term.cbreak():
+            cmd = self.term.inkey()
+        return (self.menu[cmd],)
+    
+    def quit(self) -> 'End':
+        """Command method. Quit the game of life."""
+        return End()
+    
+    def update_ui(self):
+        """Draw the UI for the core state."""
+        raise NotImplementedError()
+    
+
+class End(State):
+    """A state that terminates the game of life."""
+    def __init__(self):
+        """Initialize an instance of End."""
+        pass
+    
+    def input(self):
+        """This should never be called on an End object."""
+        raise NotEmplementedError('End objects do not get input.')
+    
+    def update_ui(self):
+        """Do not change the UI."""
+        pass
 
 
 class Start(State):
@@ -80,9 +125,41 @@ class Start(State):
     menu = 'Copyright © 2020 Paul J. Iutzi'
     prompt = 'Press any key to continue.'
     
+    def __init__(self, data: Grid = None, term: Terminal = None):
+        """Initialize a Start object.
+        
+        :param data: (Optional.) The grid object for the current 
+            game of life.
+        :param term: (Optional.) The terminal the game of life is 
+            being run in.
+        """
+        if not term:
+            term = Terminal()
+        if not data:
+            data = Grid(term.width, (term.height - 3) * 2)
+        super().__init__(data, term)
+    
+    def input(self) -> _Command:
+        """Return a Core object."""
+        with self.term.cbreak():
+            self.term.inkey()
+        return ('run',)
+    
+    def run(self) -> Core:
+        """Return a Core object to start the game of life."""
+        return Core(self.data, self.term)
+    
     def update_ui(self):
         """Draw the initial display state."""
         self._draw_state()
         self._draw_rule()
         self._draw_commands(self.menu)
         self._draw_prompt(self.prompt)
+
+
+def main():
+    state = Start()
+    state.update_ui()
+    while not isinstance(state, End):
+        cmd, *args = state.input()
+        state = getattr(state, cmd)(*args)
--- tests/test_sui.py
+++ tests/test_sui.py
@@ -17,6 +17,69 @@ loc = '\x1b[{};{}H'
 
 
 # Test cases.
+class CoreTestCase(ut.TestCase):
+    def _make_core(self):
+        return sui.Core(grid.Grid(3, 3), blessed.Terminal())
+    
+    def test_init_with_parameters(self):
+        """Core.__init__() should accept given parameters and use them 
+        as the initial values for the relevant attributes.
+        """
+        exp = {
+            'data': grid.Grid(3, 3),
+            'term': blessed.Terminal(),
+        }
+        state = sui.Core(**exp)
+        act = {
+            'data': state.data,
+            'term': state.term,
+        }
+        self.assertDictEqual(exp, act)
+    
+    @patch('blessed.Terminal.cbreak')
+    @patch('blessed.Terminal.inkey')
+    def _get_input_response(self, sym_input, mock_inkey, _):
+        mock_inkey.return_value = sym_input
+        state = self._make_core()
+        return state.input()
+    
+    def test_input_quit(self):
+        """Core.input() should return the quit command when selected 
+        by the user.
+        """
+        exp = ('quit',)
+        act = self._get_input_response('q')
+        self.assertTupleEqual(exp, act)
+    
+    def test_quit(self):
+        """Core.quit() should return an End object."""
+        exp = sui.End
+        state = self._make_core()
+        act = state.quit()
+        self.assertIsInstance(act, exp)
+
+
+class EndTestCase(ut.TestCase):
+    def test_init_without_parameters(self):
+        """End.__init__() should not require parameters."""
+        exp = sui.End
+        act = exp()
+        self.assertIsInstance(act, exp)
+
+
+class mainTestCase(ut.TestCase):
+    @patch('life.sui.print')
+    @patch('blessed.Terminal.cbreak')
+    @patch('blessed.Terminal.inkey')
+    def test_full_loop(self, mock_inkey, _, __):
+        """The main() loop should start and end the game of life."""
+        mock_inkey.side_effect = (' ', 'q')
+        
+        # The test fails if the input above doesn't terminate the 
+        # loop in main().
+        sui.main()
+
+
 class StartTestCase(ut.TestCase):
     def _dicteq(self, expected, actual):
         self.assertDictEqual(expected, actual)
@@ -54,6 +117,37 @@ class StartTestCase(ut.TestCase):
         }
         self._dicteq(exp, act)
     
+    @patch('blessed.Terminal.cbreak')
+    @patch('blessed.Terminal.inkey', return_value=' ')
+    def test_input(self, _, __):
+        """Start.input() should return the run command if any key is 
+        pressed."""
+        exp = ('run',)
+        state = sui.Start()
+        act = state.input()
+        self.assertTupleEqual(exp, act)
+        
+    
+    def test_run(self):
+        """Start.run() should always return a Core object 
+        initialized with the Start object's grid and term.
+        """
+        exp_class = sui.Core
+        exp_attrs = {
+            'data': grid.Grid(3, 3),
+            'term': blessed.Terminal(),
+        }
+        
+        state = sui.Start(**exp_attrs)
+        act = state.run()
+        act_attrs = {
+            'data': act.data,
+            'term': act.term,
+        }
+        
+        self.assertIsInstance(act, exp_class)
+        self.assertDictEqual(exp_attrs, act_attrs)
+    
     @patch('life.sui.print')
     def test_update_ui(self, mock_print):
         """Start.update_ui() should draw the initial UI."""
@@ -73,3 +167,4 @@ class StartTestCase(ut.TestCase):
         act = mock_print.mock_calls
         
         self._listeq(exp, act)
+    
